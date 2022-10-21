14c25d0ff457ef511ac869a796ca266e98ee8216
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2022-10-21 15:14:15 +0200

    refactor

--- app/Ocr.fs
+++ app/Ocr.fs
@@ -3,14 +3,16 @@ module ocr
 open Characters
 
 let parse (input: string)  =
-    if input = one then Some 1
-    elif input = two then Some 2
-    elif input = three then Some 3
-    elif input = four then Some 4
-    elif input = five then Some 5
-    elif input = six then Some 6
-    elif input = seven then Some 7
-    elif input = eight then Some 8
-    elif input = nine then Some 9
-    elif input = zero then Some 0
-    else None
+    match input with
+    | x when x = one -> Some 1
+    | x when x = two -> Some 2
+    | x when x = three -> Some 3
+    | x when x = four -> Some 4
+    | x when x = five -> Some 5
+    | x when x = six -> Some 6
+    | x when x = seven -> Some 7
+    | x when x = eight -> Some 8
+    | x when x = nine -> Some 9
+    | x when x = zero -> Some 0
+    | _ -> None
+    
