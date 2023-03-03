956dce00469e6d62a705d7cf04afa4149f9bacac
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-03 15:40:11 +0100

    wip

--- app/Ocr.fs
+++ app/Ocr.fs
@@ -5,16 +5,7 @@ open Characters
 
 let parseDigit (input: string)  =
     match input with
-    | x when x = one -> Some 1
-    | x when x = two -> Some 2
-    | x when x = three -> Some 3
-    | x when x = four -> Some 4
-    | x when x = five -> Some 5
-    | x when x = six -> Some 6
-    | x when x = seven -> Some 7
-    | x when x = eight -> Some 8
-    | x when x = nine -> Some 9
-    | x when x = zero -> Some 0
+    | Characters.one -> Some 1
     | _ -> None
     
 let parse (input: string) = 
