3ff04f425401ce07dcdbeaa23a1daf00d7b62299
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-03 14:30:47 +0100

    Introduce sann=itize input function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-def add(input):
+def sanitize_input(input):
     if input.startswith("//"):
         custom_deli = input[2]
-        input = input[4::].replace(custom_deli, ",")
+        return input[4::].replace(custom_deli, ",")
+    return input
+def add(input):
+    input = sanitize_input(input)
 
-    token = []
     if "-" in input:
         raise Exception(f"negatives not allowed: {input}")
-    
+
+    token = []
     if input:
         token = input.replace("\n", ",").split(",")
     return sum([int(x) for x in token])
