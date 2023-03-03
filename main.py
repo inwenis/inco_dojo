ad339dc370c5edaf0d642b154e2f1dfacd1c3e32
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-03 14:31:36 +0100

    Cleanup

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ def sanitize_input(input):
         custom_deli = input[2]
         return input[4::].replace(custom_deli, ",")
     return input
-def add(input):
-    input = sanitize_input(input)
 
+def add(input):
     if "-" in input:
         raise Exception(f"negatives not allowed: {input}")
-
+    
+    input = sanitize_input(input)
+ 
     token = []
     if input:
         token = input.replace("\n", ",").split(",")
