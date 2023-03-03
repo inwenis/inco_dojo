aa264d68d2ef328c5b69d9ba73536fe13ba88c00
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-03 14:01:30 +0100

    Test 8 refactor

--- main.py
+++ main.py
@@ -2,13 +2,5 @@ def add(input):
     if not input:
         return 0
 
-    if "\n" and "," in input:
-        token = input.replace("\n", ",").split(",")
-        return sum([int(x) for x in token])
-
-    if "\n" in input:
-        token = input.split("\n")
-        return sum([int(x) for x in token])
-
-    token = input.split(",")
+    token = input.replace("\n", ",").split(",")
     return sum([int(x) for x in token])
