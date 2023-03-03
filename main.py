afc8ad03559ef31b6e86988095628cf17d8cfcac
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-03 13:45:23 +0100

    refactor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def add(input):
-    if "," in input:
-        token = input.split(",")
-        return sum([int(x) for x in token])
+    if not input:
+        return 0
 
-    if input:
-        return int(input)
-
-    return 0
+    token = input.split(",")
+    return sum([int(x) for x in token])
