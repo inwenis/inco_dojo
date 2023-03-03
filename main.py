2c45b036f1862ea4511d1ee47970c96bf0e36e0e
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-03 14:29:17 +0100

    refactor

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ def add(input):
         custom_deli = input[2]
         input = input[4::].replace(custom_deli, ",")
 
-    if not input:
-        return 0
-
+    token = []
     if "-" in input:
         raise Exception(f"negatives not allowed: {input}")
     
-    token = input.replace("\n", ",").split(",")
+    if input:
+        token = input.replace("\n", ",").split(",")
     return sum([int(x) for x in token])
