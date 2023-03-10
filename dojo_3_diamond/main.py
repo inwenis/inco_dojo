33e8e70b189a26c30d90257466807bf1d72d0f7f
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-10 13:37:34 +0100

    Test 4 refactor

--- dojo_3_diamond/main.py
+++ dojo_3_diamond/main.py
@@ -3,10 +3,6 @@ from string import ascii_uppercase
 def set_of_letters(input):
     index = ascii_uppercase.find(input)
     return ascii_uppercase[:index + 1]
-    
-    if input == "A":
-        return "A"
-    return "AB"
 
 def diamond(input):
     return input
