976863ca1b967be0c51d4a27ee07840264a44f3d
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-10 14:59:53 +0100

    some crazy shitty refactoring

--- dojo_3_diamond/main.py
+++ dojo_3_diamond/main.py
@@ -4,11 +4,6 @@ def set_of_letters(input):
     index = ascii_uppercase.find(input)
     return ascii_uppercase[:index + 1]
 
-def letters(input):
-    letters = set_of_letters(input)
-    rev = "".join(reversed(letters))
-    return letters + rev[1:]
-
 def indentNoOfSpaces(inputLetter, rowLetter):
     return ord(inputLetter) - ord(rowLetter)
 
@@ -16,20 +11,24 @@ def add_new_lines(input):
     res = ""
     space_counter = 1
     increment = 2
-    middle = len(input)//2
-    for (idx,ch) in enumerate(input):
+    middle = len(input) - 1
+    for ch in input:
         if ch == "A":
             res += " " * indentNoOfSpaces(input[middle], ch) + ch + "\n"
         else:
             res += " " * indentNoOfSpaces(input[middle], ch) + ch + " " * space_counter + ch + "\n"
-            if idx == middle:
-                increment = -2
             space_counter = space_counter + increment
 
     return res[:-1]
 
 def diamond(input):
-    return letters(input)
+    return set_of_letters(input)
+
+def diamond(letter):
+    l = set_of_letters(letter)
+    top = add_new_lines(l)
+    bot = "\n".join(list(reversed(top.split('\n')))[1:])
+    return top + "\n" + bot
 
 
-print(add_new_lines(letters("C")))
+print(diamond("I"))
--- dojo_3_diamond/test.py
+++ dojo_3_diamond/test.py
@@ -2,10 +2,6 @@ import unittest
 from main import *
 
 class TestMonkey(unittest.TestCase):
-    def test_for_a_returns_a(self):
-        result = diamond("A")
-        self.assertEqual("A", result)
-
     def test_return_correct_set_of_letters(self):
         result = set_of_letters("B")
         self.assertEqual("AB", result)
@@ -18,10 +14,6 @@ class TestMonkey(unittest.TestCase):
         result = set_of_letters("C")
         self.assertEqual("ABC", result)
 
-    def test_return_correct_letters_in_order(self):
-        result = letters("C")
-        self.assertEqual("ABCBA", result)
-
     def test_return_correct_diamond_A(self):
         result = indentNoOfSpaces("A", "A")
         self.assertEqual(0, result)
@@ -31,7 +23,7 @@ class TestMonkey(unittest.TestCase):
         self.assertEqual(2, result)
 
     def test_return_correct_diamond_up_to_c(self):
-        result = add_new_lines(letters("C"))
+        result = diamond("C")
         self.assertEqual("  A\n B B\nC   C\n B B\n  A", result)
 
 if __name__ == "__main__":
