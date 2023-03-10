aa6bc498c62ab09437b7d80d3e5e3ee54c433e61
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2023-03-10 15:01:26 +0100

    some more crazy shitty refactoring

--- dojo_3_diamond/main.py
+++ dojo_3_diamond/main.py
@@ -7,26 +7,22 @@ def set_of_letters(input):
 def indentNoOfSpaces(inputLetter, rowLetter):
     return ord(inputLetter) - ord(rowLetter)
 
-def add_new_lines(input):
+def diamond_top(letter, input):
     res = ""
     space_counter = 1
     increment = 2
-    middle = len(input) - 1
     for ch in input:
         if ch == "A":
-            res += " " * indentNoOfSpaces(input[middle], ch) + ch + "\n"
+            res += " " * indentNoOfSpaces(letter, ch) + ch + "\n"
         else:
-            res += " " * indentNoOfSpaces(input[middle], ch) + ch + " " * space_counter + ch + "\n"
+            res += " " * indentNoOfSpaces(letter, ch) + ch + " " * space_counter + ch + "\n"
             space_counter = space_counter + increment
 
     return res[:-1]
 
-def diamond(input):
-    return set_of_letters(input)
-
 def diamond(letter):
-    l = set_of_letters(letter)
-    top = add_new_lines(l)
+    letters = set_of_letters(letter)
+    top = diamond_top(letter, letters)
     bot = "\n".join(list(reversed(top.split('\n')))[1:])
     return top + "\n" + bot
 
